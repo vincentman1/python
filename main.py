3497dcca0db79189814b53d1948d2416669b4628
vincentman1 <60392350+vincentman1@users.noreply.github.com>
2020-05-21 16:42:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(pythontest)
+app = Flask(main.py)
 
 @app.route("/")
 def home():
     return "Hello, world"
-    
-if pythontest == "pythontest":
+
+if main.py == "main.py":
     app.run(debug=True)
